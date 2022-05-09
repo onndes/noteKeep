90d21549a08332ec13e1d6ffeb75a7f750a1c6b5
Vlad Vylotnilov <vladvyljotnikov@gmail.com>
2022-05-09 14:26:50 +0300

    added / functional selected note

--- assets/colorApp.js
+++ assets/colorApp.js
@@ -4,10 +4,13 @@ export default function getColorApp(theme = "dark") {
             backgroundMain: "#181c1f",
             backgroundAction: "#1f282d",
             light: "#eeeff1",
-            lightTwo: "#909497",
+            lightTwo: "#73777a",
+            selected: "#69d5ff",
+            pressIn: "#2d3134"
         };
     } else if (theme === "light") {
         return {};
     }
     return {};
 }
+3;
--- src/components/HomeScreen/NotesList.js
+++ src/components/HomeScreen/NotesList.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Pressable } from "react-native";
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 
 import getColorApp from "../../../assets/colorApp";
 
 export default function NotesList() {
     const [notes, setNotes] = React.useState([]);
+    const [selectedNotes, setSelectedNotes] = React.useState([]);
+    const [pressInNote, setPressInNote] = React.useState(null);
     const { getItem: getItemNotes } = useAsyncStorage("notes");
 
     const readItemFromStorage = async () => {
@@ -16,17 +18,42 @@ export default function NotesList() {
 
     React.useEffect(() => {
         readItemFromStorage();
-    }, [notes]);
+    }, []);
+
+    const handlePressNote = (noteId) => {
+        setSelectedNotes(selectedNotes.filter((id) => id !== noteId));
+    };
 
     return (
         <View>
             {!!notes.length ? (
                 notes.map((note) => {
+                    const selectedStyle =
+                        selectedNotes.find((id) => id === note.id) &&
+                        styles.noteContainerSelected;
+
+                    const pressInStyle =
+                        pressInNote === note.id && styles.notePressIn;
                     return (
-                        <View key={note.id} style={styles.noteContainer}>
-                            <Text style={styles.title}>{note.title}</Text>
-                            <Text style={styles.text}>{note.text}</Text>
-                        </View>
+                        <Pressable
+                            key={note.id}
+                            onLongPress={() =>
+                                setSelectedNotes([...selectedNotes, note.id])
+                            }
+                            onPress={() => handlePressNote(note.id)}
+                            onPressIn={() => setPressInNote(note.id)}
+                            onPressOut={() => setPressInNote(null)}>
+                            <View
+                                key={note.id}
+                                style={[
+                                    styles.noteContainer,
+                                    selectedStyle,
+                                    pressInStyle,
+                                ]}>
+                                <Text style={styles.title}>{note.title}</Text>
+                                <Text style={styles.text}>{note.text}</Text>
+                            </View>
+                        </Pressable>
                     );
                 })
             ) : (
@@ -45,5 +72,12 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 18,
         marginBottom: 12,
+    },
+    noteContainerSelected: {
+        borderWidth: 3,
+        borderColor: getColorApp().selected,
+    },
+    notePressIn: {
+        backgroundColor: getColorApp().pressIn,
     },
 });
